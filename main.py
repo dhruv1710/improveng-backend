e51320548f397cc47a2fc2290d5e152403e5ddce
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2024-04-02 14:27:20 +0530

    AI21 grammar and text

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 import base64
 import uvicorn
 import io
+import requests
 from fastapi.middleware.cors import CORSMiddleware
 import boto3
 boto3.setup_default_session(region_name='ap-south-1',aws_access_key_id='AKIAVJMOT2XBVTZA6SNN',
@@ -39,3 +40,35 @@ async def extract_text(file: UploadFile = File(...)):
         if k['BlockType'] == 'WORD':
             textList.append(k['Text'])
     return {'text':textList}
+
+@app.get('/grammar')
+def grammar_corrections(text: str):
+    url = "https://api.ai21.com/studio/v1/gec"
+    payload = { "text": text }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer RX92sajEHPI95UryWjQdFNVeQ343CY1Q"
+    }
+
+    response = requests.post(url,json=payload, headers=headers)
+    print(response.json())
+    return response.json()
+
+@app.get('/text-improvements')
+def text_improvements(text: str):
+    url = "https://api.ai21.com/studio/v1/improvements"
+
+    payload = {
+        "types": ["fluency", "vocabulary/specificity", "vocabulary/variety", "clarity/short-sentences", "clarity/conciseness"],
+        "text": text
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": "Bearer RX92sajEHPI95UryWjQdFNVeQ343CY1Q"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+    return response.json()
